2b8a8b8ffd3e4af11a165f4db040eaba3bfe14c4
zhangmingming <zhangmingming@qq.com>
2019-01-10 10:16:40 +0800

    Site updated: 2019-01-10 10:16:27

--- static/api/js/conf/const.js
+++ static/api/js/conf/const.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 10 2019 09:14:21 GMT+0800 (中国标准时间)
+// build time:Thu Jan 10 2019 10:15:47 GMT+0800 (中国标准时间)
 !window._bd_share_is_recently_loaded&&window._bd_share_main.F.module("conf/const",function(t,a,c){a.CMD_ATTR="data-cmd",a.CONFIG_TAG_ATTR="data-tag",a.URLS={likeSetUrl:"http://like.baidu.com/set",commitUrl:"http://s.share.baidu.com/commit",jumpUrl:"http://s.share.baidu.com",mshareUrl:"http://s.share.baidu.com/mshare",emailUrl:"http://s.share.baidu.com/sendmail",nsClick:"http://nsclick.baidu.com/v.gif",backUrl:"http://s.share.baidu.com/back",shortUrl:"http://dwz.cn/v2cut.php"}});
 //rebuild by neat 
--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 10 2019 09:14:21 GMT+0800 (中国标准时间)
+// build time:Thu Jan 10 2019 10:15:47 GMT+0800 (中国标准时间)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
