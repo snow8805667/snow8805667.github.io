2e105a23b2e042e7f64ab1721c23524208cb6286
zhangmingming <zhangmingming@qq.com>
2019-01-10 09:15:23 +0800

    Site updated: 2019-01-10 09:15:13

--- static/api/js/trans/data.js
+++ static/api/js/trans/data.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 08 2019 14:48:51 GMT+0800 (中国标准时间)
+// build time:Thu Jan 10 2019 09:14:21 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
--- static/api/js/trans/trans_bdxc.js
+++ static/api/js/trans/trans_bdxc.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 08 2019 14:48:52 GMT+0800 (中国标准时间)
+// build time:Thu Jan 10 2019 09:14:21 GMT+0800 (中国标准时间)
 window._bd_share_main.F.module("trans/trans_bdxc",function(e,t){var n=function(){var e=window,t=document,n="_bdXC",a;e[n]?window._bdXC_loaded&&e[n].reInit():(a=t.createElement("script"),a.setAttribute("charset","utf-8"),a.src="http://xiangce.baidu.com/zt/collect/mark.js?"+(new Date).getTime(),t.getElementsByTagName("head")[0].appendChild(a))};t.run=n});
 //rebuild by neat 
